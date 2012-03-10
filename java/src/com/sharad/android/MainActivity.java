8cc31e44da78b86a3f173d56d2fcf544604c62e9
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-10 04:21:17 -0600

    fully working

--- java/src/com/sharad/android/MainActivity.java
+++ java/src/com/sharad/android/MainActivity.java
@@ -1,17 +1,13 @@
 package com.sharad.android;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import com.flotype.bridge.*;
 
-import com.flotype.bridge.Bridge;
-import com.flotype.bridge.BridgeEventHandler;
 import com.flotype.bridge.Service;
 
 public class MainActivity extends Activity {
@@ -19,16 +15,38 @@ public class MainActivity extends Activity {
 	PushClient pusher;
 	TextView view;
 	EditText text;
-
+	String name;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		
+		
+// Set an EditText view to get user input 
+		final EditText input = new EditText(this);
+		alert.setView(input);
+		alert.setTitle("Enter your name");
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					name = input.getText().toString();
+					// Do something with value!
+				}
+			});
+		
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					// Canceled.
+				}
+			});
+		
+		alert.show();
 		view = (TextView) findViewById(R.id.view);
 		text = (EditText) findViewById(R.id.text);
 		// final Bridge bridge = new Bridge().setApiKey("abcdefgh");
 		// bridge.connect();
+		final Handler handler = new Handler();
 		if (android.os.Build.VERSION.SDK_INT > 9) {
 			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
 					.permitAll().build();
@@ -36,13 +54,17 @@ public class MainActivity extends Activity {
 		}
 		final Bridge bridge = new Bridge().setApiKey("abcdefgh");
 		bridge.setEventHandler(new BridgeEventHandler() {
-
+				public Runnable display= new Runnable(){
+					public void run(){
+						updateMessage();
+					}
+				};
 			public void onReady() {
 				bridge.joinChannel("andruino", new Service() {
 					public void push(String name, String message) {
 						System.out.println(name + ": " + message);
-						current += name + ": " + message + "\n";
-						view.setText(name + ": " + message + "\n");
+						current+=name+": "+message+"\n";
+						handler.post(display);
 					}
 				});
 				pusher = bridge.getChannel("andruino", PushClient.class);
@@ -77,10 +99,13 @@ public class MainActivity extends Activity {
 
 		bridge.connect();
 	}
+	public void updateMessage() {
+		view.setText(current);
+	}
 
 	public void sendMessage() {
 		System.out.println("SENDING");
-		pusher.push("Android", ((EditText) findViewById(R.id.text)).getText()
+		pusher.push(name, ((EditText) findViewById(R.id.text)).getText()
 				.toString());
 		((EditText) findViewById(R.id.text)).setText("");
 	}
