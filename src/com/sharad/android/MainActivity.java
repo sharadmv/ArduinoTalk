9edd9933d61dc6634df028ff3cda45c7cfb3a81a
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-09 19:25:58 -0600

    first commit

---
+++ src/com/sharad/android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sharad.android;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
