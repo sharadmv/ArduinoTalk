92ab379c1e3e04d77ad7033fdfd07a85d292b932
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-09 20:45:25 -0800

    fixed bridge connectivity issue

--- java/src/com/sharad/android/MainActivity.java
+++ java/src/com/sharad/android/MainActivity.java
@@ -2,8 +2,12 @@ package com.sharad.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.flotype.bridge.Bridge;
@@ -19,23 +23,65 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		final Bridge bridge = new Bridge().setApiKey("YkYztDEV");
+		// final Bridge bridge = new Bridge().setApiKey("abcdefgh");
+		// bridge.connect();
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+		}
+		final Bridge bridge = new Bridge().setApiKey("abcdefgh");
+		bridge.setEventHandler(new BridgeEventHandler() {
+
+			public void onReady() {
+				bridge.joinChannel("andruino", new Service() {
+					public void push(String name, String message) {
+						System.out.println(name + ": " + message);
+						current += name + ": " + message + "\n";
+						TextView editText = (TextView) findViewById(R.id.view);
+						editText.setText(name + ": " + message + "\n",
+								TextView.BufferType.EDITABLE);
+					}
+				});
+				pusher = bridge.getChannel("andruino", PushClient.class);
+				findViewById(R.id.button).setOnClickListener(
+						new OnClickListener() {
+
+							public void onClick(View arg0) {
+								sendMessage();
+							}
+
+						});
+				((EditText) findViewById(R.id.text))
+						.setOnKeyListener(new OnKeyListener() {
+
+							public boolean onKey(View arg0, int keyCode,
+									KeyEvent event) {
+								if (event.getAction() == KeyEvent.ACTION_DOWN) {
+									switch (keyCode) {
+									case KeyEvent.KEYCODE_DPAD_CENTER:
+									case KeyEvent.KEYCODE_ENTER:
+										sendMessage();
+										return true;
+									default:
+										break;
+									}
+								}
+								return false;
+							}
+
+						});
+			}
+
+		});
+
 		bridge.connect();
-		bridge.joinChannel("andruino", new Service() {
-			public void push(String name, String message) {
-				current += name + ": " + message + "\n";
-				((TextView) findViewById(R.id.view)).setText(current);
-			}
-		});
-		pusher = bridge.getChannel("andruino", PushClient.class);
-		findViewById(R.id.button).setOnClickListener(new OnClickListener() {
+	}
 
-			public void onClick(View arg0) {
-				System.out.println("SENDING");
-				pusher.push("Android", ((TextView) findViewById(R.id.text))
-						.getText().toString());
-			}
-
-		});
+	public void sendMessage() {
+		System.out.println("SENDING");
+		pusher.push("Android", ((EditText) findViewById(R.id.text)).getText()
+				.toString());
+		((EditText) findViewById(R.id.text)).setText("");
 	}
 }
