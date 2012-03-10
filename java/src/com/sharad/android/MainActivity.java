c6c27fde8f2e98a3ad2e1b77e5cd880291e487c1
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-09 18:55:43 -0800

    added chat capabilities

--- java/src/com/sharad/android/MainActivity.java
+++ java/src/com/sharad/android/MainActivity.java
@@ -2,18 +2,44 @@ package com.sharad.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.flotype.bridge.Bridge;
+import com.flotype.bridge.BridgeEventHandler;
+import com.flotype.bridge.Service;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Bridge bridge = new Bridge();
-		
-    }
+public class MainActivity extends Activity {
+	String current = "";
+	PushClient pusher;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		final Bridge bridge = new Bridge().setApiKey("abcdefgh");
+		bridge.connect();
+		bridge.joinChannel("andruino", new Service() {
+			public void push(String name, String message) {
+				current += name + ": " + message + "\n";
+				((TextView) findViewById(R.id.view)).setText(current);
+			}
+		});
+		bridge.setEventHandler(new BridgeEventHandler() {
+
+			public void onReady() {
+				pusher = bridge.getChannel("andruino", PushClient.class);
+			}
+
+		});
+		findViewById(R.id.button).setOnClickListener(new OnClickListener(){
+
+			public void onClick(View arg0) {
+				pusher.push("Android", ((TextView)findViewById(R.id.text)).getText().toString());	
+			}
+			
+		});
+	}
 }
